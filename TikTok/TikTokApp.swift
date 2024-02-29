b21db4aa9303749e4d223250a5720d3bb08bc959
abdulmajit <abkubatt@gmail.com>
2024-02-29 13:19:47 +0600

    Initial Commit

---
+++ TikTok/TikTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokApp.swift
+//  TikTok
+//
+//  Created by Abdulmajit Kubatbekov on 29/2/24.
+//
+
+import SwiftUI
+
+@main
+struct TikTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
