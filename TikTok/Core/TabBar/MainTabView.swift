0b2e77fc1c16628db33be67b9e5a3f3ffa7cd401
abdulmajit <abkubatt@gmail.com>
2024-03-05 08:51:59 +0600

    setup tabbar

---
+++ TikTok/Core/TabBar/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  TikTok
+//
+//  Created by Abdulmajit Kubatbekov on 5/3/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
