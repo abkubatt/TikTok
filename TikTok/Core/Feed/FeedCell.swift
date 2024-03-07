985549bfcd48997973ff772d4f4305c3cc9fdd25
abdulmajit <abkubatt@gmail.com>
2024-03-07 11:51:09 +0600

    setup layout of feed view and launch screen

--- TikTok/Core/Feed/FeedCell.swift
+++ TikTok/Core/Feed/FeedCell.swift
@@ -8,11 +8,97 @@
 import SwiftUI
 
 struct FeedCell: View {
+    let post: Int
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Rectangle()
+                .fill(.pink)
+                .containerRelativeFrame([.horizontal, .vertical])
+                .overlay {
+                    Text("Post \(post)")
+                        .foregroundStyle(.white)
+                }
+            
+            VStack {
+                Spacer()
+                
+                HStack(alignment: .bottom) {
+                    VStack(alignment: .leading) {
+                        Text("abkubatt")
+                            .fontWeight(.semibold)
+                        
+                        Text("I am going to be CEO of Apple in 2035!")
+                    }
+                    .foregroundStyle(.white)
+                    .font(.subheadline)
+                    
+                    Spacer()
+                    
+                    VStack(spacing: 20) {
+                        Circle()
+                            .frame(width: 45, height: 45)
+                            .foregroundStyle(.gray)
+                        
+                        
+                        Button {
+                            
+                        } label: {
+                            VStack {
+                                Image(systemName: "heart.fill")
+                                    .resizable()
+                                    .frame(width: 28, height: 28)
+                                    .foregroundStyle(.white)
+                                
+                                Text("27")
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            VStack {
+                                Image(systemName: "ellipsis.bubble.fill")
+                                    .resizable()
+                                    .frame(width: 28, height: 28)
+                                    .foregroundStyle(.white)
+                                
+                                Text("3")
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "bookmark.fill")
+                                .resizable()
+                                .frame(width: 22, height: 28)
+                                .foregroundStyle(.white)
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "arrowshape.turn.up.right.fill")
+                                .resizable()
+                                .frame(width: 28, height: 28)
+                                .foregroundStyle(.white)
+                        }
+                    }
+                }
+                .padding(.bottom, 85)
+            }
+            .padding()
+        }
     }
 }
 
 #Preview {
-    FeedCell()
+    FeedCell(post: 1)
 }
