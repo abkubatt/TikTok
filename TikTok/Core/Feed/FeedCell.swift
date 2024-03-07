06c0008498e764d894810c04bb9de7220faf7028
abdulmajit <abkubatt@gmail.com>
2024-03-07 11:51:03 +0600

    setup layout of feed view and launch screen

---
+++ TikTok/Core/Feed/FeedCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCell.swift
+//  TikTok
+//
+//  Created by Abdulmajit Kubatbekov on 7/3/24.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedCell()
+}
---
+++ TikTok/Core/Feed/FeedView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedView.swift
+//  TikTok
+//
+//  Created by Abdulmajit Kubatbekov on 7/3/24.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedView()
+}
